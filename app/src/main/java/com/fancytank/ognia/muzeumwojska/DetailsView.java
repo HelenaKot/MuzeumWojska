0e6120e022a975b83c11e7fe4fb67dba6ca7769a
rilane <s10474@pjwstk.edu.pl>
2017-04-30 13:30:01 +0200

    added buttons

--- app/src/main/java/com/fancytank/ognia/muzeumwojska/DetailsView.java
+++ app/src/main/java/com/fancytank/ognia/muzeumwojska/DetailsView.java
@@ -22,17 +22,45 @@ public class DetailsView extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.web_view);
 
         getIntent().getExtras().getSerializable(DisplayListAdapter.TAG);
-        loadMapwithAdr();
+//        loadMiniMapWithAdr("52.1819961,21.0645194");
+        loadMapwithAdr("52.182242,21.065728");
         filldata(data);
     }
 
-    private void loadMapwithAdr() {
+    private void loadMapwithAdr(String addrs) {
 
         final String url = "http://maps.google.com/maps";
 
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://maps.google.com/maps?q=" + "52.1819961,21.0645194");
+        webView.loadUrl("http://maps.google.com/maps?q=" + addrs);
+    }
+
+    private void loadMiniMapWithAdr(String addrs) {
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(final WebView view, final String url) {
+                super.onPageFinished(view, url);
+                resizeWebView();
+                webView.invalidate();
+            }
+        });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://maps.googlemaps.com/maps/api/staticmap?maptype=satellite&center=" + addrs + "&zoom=18" + "&size=100x100");
+        webView.requestLayout();
+        webView.invalidate();
+    }
+
+    private void resizeWebView() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int size = webView.getLayoutParams().height;
+                webView.setLayoutParams(new LinearLayout.LayoutParams(size, size));
+            }
+        });
     }
 
     private void filldata(DisplayUnit data) {
