867ebe865b3095cb8e18f44abe5f1f52cf75f6b8
rilane <s10474@pjwstk.edu.pl>
2017-04-30 12:39:09 +0200

    added map

--- app/src/main/java/com/fancytank/ognia/muzeumwojska/DetailsView.java
+++ app/src/main/java/com/fancytank/ognia/muzeumwojska/DetailsView.java
@@ -2,31 +2,40 @@ package com.fancytank.ognia.muzeumwojska;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 
 import com.fancytank.ognia.muzeumwojska.api.model.DisplayUnit;
 import com.fancytank.ognia.muzeumwojska.list.DisplayListAdapter;
 
-public class DetailsView extends AppCompatActivity  {
+public class DetailsView extends AppCompatActivity {
     LinearLayout container;
     DisplayUnit data;
+    WebView webView;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_details_view);
-//        container = (LinearLayout) findViewById(R.id.container);
-//
-//        getIntent().getExtras().getSerializable(DisplayListAdapter.TAG);
-//
-//        MapFragment mapFragment = (MapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//
-//        filldata(data);
-//    }
-//
-//    private void filldata(DisplayUnit data) {
-//
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_view);
+        container = (LinearLayout) findViewById(R.id.container);
+        webView = (WebView) findViewById(R.id.web_view);
+
+        getIntent().getExtras().getSerializable(DisplayListAdapter.TAG);
+        loadMapwithAdr();
+        filldata(data);
+    }
+
+    private void loadMapwithAdr() {
+
+        final String url = "http://maps.google.com/maps";
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://maps.google.com/maps?q=" + "52.1819961,21.0645194");
+    }
+
+    private void filldata(DisplayUnit data) {
+
+    }
 }
